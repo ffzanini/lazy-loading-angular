391e7525229eed5b603606a20465d5fa67c1cb7b
Felipe Frantz Zanini <felipe.zanini@nave.rs>
2021-06-28 11:24:55 -0300

    added routes modules to pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'pages/customers', 
+    loadChildren: () => import('./pages/customers/customers.module').then(m => m.CustomersModule) 
+  }, 
+  { 
+    path: 'pages/orders', 
+    loadChildren: () => import('./pages/orders/orders.module').then(m => m.OrdersModule) 
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
